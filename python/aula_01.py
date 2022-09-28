fbbb73005ad82cba2a3429bc9811119156be565e
Jonatas Pedro da Silva <jonatas.pedro@academico.ifrn.edu.br>
2022-09-28 08:09:37 -0300

    pasta pra aulas de python

--- python/aula_01.py
+++ python/aula_01.py
@@ -1,12 +1,26 @@
 print("Buenos dias!")
-
+'''
 numero = 10
 print(numero)
 
 print(type(numero))
 
-# comentário 
+nome = input("Digite seu nome:")
+print("Nome digitado: ", nome)
 
+idade = input("Digite sua idade:")
+print("Idade: ", idade)
+
+preco = float(input("Digite o preço: "))
+print("Preço: ", preco)
+
+'''
+numero = int(input("Digite um número: "))
+print(numero*15/100)
+'''
+
+# comentário 
+'''
 '''
 Comentários de várias linhas
 01
