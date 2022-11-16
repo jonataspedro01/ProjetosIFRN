3c915d620ed323f2e18f3bef3e13611e97808149
Jonatas Pedro <jonatas3pedro@hotmail.com>
2022-11-16 18:12:25 -0300

    atividade

---
+++ python/WEBnaNuvem/atividade_um.py
@@ -0,0 +1,30 @@
+import requests
+
+import json
+
+SUBSCRIPTION_KEY = "61ca29eb5a654dff98b6f4bf96f2aff5"
+
+vision_service_address = "https://pythonimageanalizejonatas.cognitiveservices.azure.com/vision/v2.0/ocr?"
+address = vision_service_address + "analyze"
+
+parameters  = {'detalls':'regions',
+               'language':'pt'}
+
+
+image_path = "./flamengo.jpg"
+image_data = open(image_path, "rb").read()
+
+
+headers    = {'Content-Type': 'application/octet-stream',
+              'Ocp-Apim-Subscription-Key': SUBSCRIPTION_KEY}
+
+
+response = requests.post(address, headers=headers, params=parameters, data=image_data)
+
+
+response.raise_for_status()
+
+
+results = response.json() 
+print(json.dumps(results))
+
