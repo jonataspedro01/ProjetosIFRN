29d00c83fe7da548459196ffa3f57b235a6a7a3b
Jonatas Pedro <jonatas3pedro@hotmail.com>
2022-11-17 01:54:01 -0300

    exercícios

---
+++ python/WEBnaNuvem/exerc03_13.py
@@ -0,0 +1,14 @@
+"""
+
+numeros = 0
+impares = 0
+
+for numeros in range(15,35):
+    if numeros % 2 == 1:
+       impares = impares + 1
+print("A quantidade de números ímpares é: ", impares)
+
+"""
+for i in range(15,36):
+     if i % 2 == 1:
+      print(i*i)
---
+++ python/WEBnaNuvem/exerc03_5.py
@@ -0,0 +1,16 @@
+
+soma = 0
+impar = 1
+
+result = 1
+
+for i in range(1,31):
+     if i % 2 == 0:
+      result = result * i
+      print("Multiplicação do números pares", result)
+     else:
+      soma += impar
+      impar += 2
+      print("Soma dos números impares",soma)
+      
+      
