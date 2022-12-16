8a2464d33f7e71eea8ba0fb474cf1d367aeaa4c5
Jonatas Pedro <jonatas3pedro@hotmail.com>
2022-12-15 22:54:59 -0300

    dados das últimas 3 semanas

---
+++ python/testando.py
@@ -0,0 +1,7 @@
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digiteo segundo número: "))
+num3 = int(input("Digite o terceiro número: "))
+
+Lsta=[num1,num2, num3]
+
+print(sorted(Lsta))
